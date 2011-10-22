e64f67e853c596462cbd57d23fe5c4d0957e32c8
Brian <brian.a.hassinger@gmail.com>
2011-10-22 14:42:59 -0400

    Documentation and API structure

--- steam.js
+++ steam.js
@@ -4,6 +4,7 @@
 
   var steam = {
     host: 'api.steampowered.com',
+
     defaults: {
       format: 'json',
       steamid: '76561197960435530',
@@ -15,6 +16,7 @@
       maxlength: '300',
       version: '0001'
     },
+
     execute: function(path, callback) {
       http.get({host: this.host,
                 port: 80,
@@ -30,6 +32,7 @@
         });
       });
     },
+
     prepare: function(params, callback) {
       var params = utils.merge(this.settings, params);
       this.execute('/' + params.interface + '/' + params.method + '/v' + params.version
@@ -39,49 +42,78 @@
         callback(err, result);
       });
     },
+
     getPlayerItems: function(params, callback) {
-      var params = utils.merge(params, {interface: 'IEconItems_' + (params.gameid || this.settings.gameid),
-                                        method: 'GetPlayerItems',
-                                        methodSpecific: '&steamid=' + (params.steamid || this.settings.steamid)
-      });
-      this.prepare(params, callback);
+      if(arguments.length === 1) {
+        callback = arguments[0];
+        params = {};
+      }
+      this.prepare(utils.merge(params, {
+        interface: 'IEconItems_' + (params.gameid || this.settings.gameid),
+        method: 'GetPlayerItems',
+        methodSpecific: '&steamid=' + (params.steamid || this.settings.steamid)
+      }), callback);
     },
+
     getSchema: function(params, callback) {
-      var params = utils.merge(params, {interface: 'IEconItems_' + (params.gameid || this.settings.gameid),
-                                        method: 'GetSchema',
-                                        methodSpecific: '&language=' + (params.language || this.settings.language)
-      });
-      this.prepare(params, callback);
+      if(arguments.length === 1) {
+        callback = arguments[0];
+        params = {};
+      }
+      this.prepare(utils.merge(params, {
+        interface: 'IEconItems_' + (params.gameid || this.settings.gameid),
+        method: 'GetSchema',
+        methodSpecific: '&language=' + (params.language || this.settings.language)
+      }), callback);
     },
+
     getNewsForApp: function(params, callback) {
-      var params = utils.merge(params, {interface: 'ISteamNews',
-                                        method: 'GetNewsForApp',
-                                        methodSpecific: '&appid=' + (params.appid || this.settings.appid)
-                                                      + '&count=' + (params.count || this.settings.count)
-                                                      + '&maxlength=' + (params.maxlength || this.settings.maxlength)
-      });
-      this.prepare(params, callback);
+      if(arguments.length === 1) {
+        callback = arguments[0];
+        params = {};
+      }
+      this.prepare(utils.merge(params, {
+        interface: 'ISteamNews',
+        method: 'GetNewsForApp',
+        methodSpecific: '&appid=' + (params.appid || this.settings.appid)
+                      + '&count=' + (params.count || this.settings.count)
+                      + '&maxlength=' + (params.maxlength || this.settings.maxlength)
+      }), callback);
     },
+
     getGlobalAchievementPercentagesForApp: function(params, callback) {
-      var params = utils.merge(params, {interface: 'ISteamUserStats',
-                                        method: 'GetGlobalAchievementPercentagesForApp',
-                                        methodSpecific: '&gameid=' + (params.gameid || this.settings.gameid)
-      });
-      this.prepare(params, callback);
+      if(arguments.length === 1) {
+        callback = arguments[0];
+        params = {};
+      }
+      this.prepare(utils.merge(params, {
+        interface: 'ISteamUserStats',
+        method: 'GetGlobalAchievementPercentagesForApp',
+        methodSpecific: '&gameid=' + (params.gameid || this.settings.gameid)
+      }), callback);
     },
     getPlayerSummaries: function(params, callback) {
-      var params = utils.merge(params, {interface: 'ISteamUser',
-                                        method: 'GetPlayerSummaries',
-                                        methodSpecific: '&steamids=' + (params.steamids ? params.steamids.toString() : this.settings.steamids.toString())
-      });
-      this.prepare(params, callback);
+      if(arguments.length === 1) {
+        callback = arguments[0];
+        params = {};
+      }
+      this.prepare(utils.merge(params, {
+        interface: 'ISteamUser',
+        method: 'GetPlayerSummaries',
+        methodSpecific: '&steamids=' + (params.steamids ? params.steamids.toString() : this.settings.steamids.toString())
+      }), callback);
     }
     
   };
 
-  exports.createClient = function(apikey, options) {
-    steam.apikey = apikey;
-    steam.settings = utils.merge(steam.defaults, options);
+  exports.createClient = function(options, apikey) {
+    if(arguments.length === 2) {
+      steam.apikey = apikey;
+      steam.settings = utils.merge(steam.defaults, options);
+    } else {
+      steam.apikey = arguments[0];
+      steam.settings = steam.defaults;
+    }
     return steam;
   };
 })();
